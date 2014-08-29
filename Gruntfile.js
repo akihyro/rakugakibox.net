163aacc6b14151f125e80ff08d690a2b6548a16f
akihyro <akihyro@users.noreply.github.com>
2014-08-29 12:11:40 +0900

    CSS オーサリングフレームワークに compass を使う

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,19 @@
 module.exports = function(grunt) {
+
+  // NPMタスクをロードする
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+  // 設定を初期化する
   grunt.initConfig({
+    compass: {
+      dist: {
+        options: {
+          bundleExec: true,
+          sassDir: "styles.src",
+          cssDir: "styles"
+        }
+      }
+    }
   });
+
 };
