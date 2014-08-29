62ea8ef12735929120da3c5862b88bbe5416247e
akihyro <akihyro@users.noreply.github.com>
2014-08-29 23:01:57 +0900

    外部リンクは別ウィンドウで開く

--- src/scripts/rakugaki-box.net.js
+++ src/scripts/rakugaki-box.net.js
@@ -2,4 +2,15 @@
 
 !function($) {
 
+    // PC向けページのみ適用する
+    if (!/^pc$/i.test($("html").data("device"))) {
+        return;
+    }
+
+    // 外部リンクは別ウィンドウで開く
+    $(document).on("click contextmenu touchstart",
+        "a[href^=http]:not([target]):not([href*='" + location.hostname + "'])",
+        function(event) { $(event.currentTarget).attr("target", "_blank"); }
+    );
+
 }($);
