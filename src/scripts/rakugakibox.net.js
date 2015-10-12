17289feed84e93c3c12b8febcaeb8f026bb43f17
Akihiro Kondo <akihyro@gmail.com>
2015-10-12 17:57:49 +0900

    remake: スクリプトを作成

--- src/scripts/rakugakibox.net.js
+++ src/scripts/rakugakibox.net.js
@@ -1,3 +1,28 @@
 /*!===================================================================================================================
  * rakugakibox.net
  *====================================================================================================================*/
+
+var $ = require("jquery");
+
+// 外部リンクは別ウィンドウで開く
+$(document).on(
+    "click contextmenu touchstart",
+    "a[href^=http]:not([target]):not([href*='" + location.hostname + "'])",
+    function(event) {
+        $(event.currentTarget).attr("target", "_blank");
+    }
+);
+
+// アイキャッチイメージリンクはエントリページを開く
+$(document).on(
+    "click contextmenu touchstart",
+    "#main .entry .entry-inner .entry-content > p:first-child span[itemtype='http://schema.org/Photograph'] a",
+    function(event) {
+        var target = $(event.currentTarget);
+        var title = target.parents(".entry-inner").first().find(".entry-header .entry-title a");
+        if (title) {
+            target.attr("href", title.attr("href"));
+            target.removeAttr("target");
+        }
+    }
+);
